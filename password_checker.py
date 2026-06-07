3281ed3a2ec8d7de8a0866e906fb8e312cf4d488
Nisha M <nishamurugesan28@gmail.com>
2026-06-07 14:33:06 +0530

    Initial upload of Password Strength Checker

---
+++ password_checker.py
@@ -0,0 +1,21 @@
+ 
+password = input("Enter your password: ")
+has_upper=False
+has_lower=False
+has_digit=False
+has_special=False
+for char in password:
+    if char.isupper():
+        has_upper=True
+    if char.islower():
+        has_lower=True
+    if char.isdigit():
+        has_digit=True
+    if char in "!@#$%^&*":
+        has_special=True
+if len(password)>=8 and has_upper and has_lower and has_digit and has_special:
+    print("strong password:")
+else:
+    print("weak password:")
+
+
